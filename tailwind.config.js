3abb8eb0e4988be5a18f4e5984fbf58f1e49446b
Henriette <mhenriette00@gmail.com>
2022-09-22 09:53:22 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            // backgroundImage: {
+            //     'hero': "url('img/M3-Homepage-Desktop-LHD.jpeg')",
+
+            // },
+            fontFamily: {
+
+                gotham: ['Gotham Book'],
+            },
+            colors: {
+                myzinc: '#3c3f43',
+                mywhite: '#e7e7e5',
+                myblack: '#171a20'
+
+            },
+
+
+        },
+    },
+    plugins: [],
+}
